ddb9398359605483e2dcfafaeef2e9dacbeb4b10
Mike-Fernandez <0014558@uca.edu.sv>
2019-11-01 11:28:44 -0600

    Updates

---
+++ precache-manifest.7082811605b57aacbb9ebd8747c8877e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5944ae87a27a883d5a932c3112765588",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "818e323dad04155ac5cf",
+    "url": "/tictactoe/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "5fc33aed0e7becc2a37c",
+    "url": "/tictactoe/static/js/2.52dd166e.chunk.js"
+  },
+  {
+    "revision": "818e323dad04155ac5cf",
+    "url": "/tictactoe/static/js/main.8a0ae640.chunk.js"
+  },
+  {
+    "revision": "1dd1878a39f419eb6036",
+    "url": "/tictactoe/static/js/runtime-main.868a150c.js"
+  }
+]);
